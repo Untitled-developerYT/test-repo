4dd278d6c5fa2f70c4bcbd0923a9c31252cf6925
Untitled-developerYT <102920258+Untitled-developerYT@users.noreply.github.com>
2025-02-05 21:22:18 +0000

    Create purge.ts

---
+++ commands/purge.ts
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Purge a certain amount of messages')
+        .addIntegerOption(option => 
+            option.setName('amount')
+            .setDescription('The amount of messages to purge')
+            .setRequired(true)),
+        
+                    
+    async execute(interaction) {
+        const amount = interaction.options.getInteger('amount');
+        if (amount < 1 || amount > 100) {
+            return interaction.reply({ content: 'You must provide a number between 1 and 100', ephemeral: true });
+        }
+        interaction.channel.bulkDelete(amount, true).catch(err => {
+            console.error(err);
+            interaction.reply({ content: 'There was an error trying to purge messages in this channel!', ephemeral: true });
+        });
+        interaction.reply({ content: `Successfully purged ${amount} messages!`, ephemeral: true });
+    },
+};
