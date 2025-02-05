a04fa849b024be2e6dfd777e23bf8ec31d02af7f
Untitled-developerYT <102920258+Untitled-developerYT@users.noreply.github.com>
2025-02-05 21:38:56 +0000

    Update purge.ts as the devs intended

--- commands/purge.ts
+++ commands/purge.ts
@@ -1,3 +1,54 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { executeCommand } from "@/types";
+
+// to add a command go to ./commands folder and create a new ts file
+
+// the command title/name should match the command.ts file for
+//      ex. for tutorialhere command you should name the file tutorialhere.ts
+
+// Don't change register and execute variable names
+export const register = new SlashCommandBuilder()
+  .setName('purge')
+    .setDescription('Purge a certain amount of messages')
+    .addIntegerOption(option => 
+        option.setName('amount')
+        .setDescription('The amount of messages to purge')
+        .setRequired(true));
+
+export const execute: executeCommand = async (interaction) => {
+  // You have access to do interaction object
+  // https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
+
+  // Do your bot logic here
+  // You can even connect to a database
+
+  // you should return a APIInteractionResponse
+  // https://discord-api-types.dev/api/discord-api-types-v10#APIApplicationCommandInteraction
+  const amount = interaction.options.getInteger('amount');
+        if (amount < 1 || amount > 100) {
+            return { type: 4, data: { content: 'You must provide a number between 1 and 100', },
+        }
+        interaction.channel.bulkDelete(amount, true).catch(err => {
+            console.error(err);
+            //interaction.reply({ content: 'There was an error trying to purge messages in this channel!', ephemeral: true });
+        });
+        //interaction.reply({ content: `Successfully purged ${amount} messages!`, ephemeral: true });
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 const { SlashCommandBuilder } = require('discord.js');
 import { executeCommand } from "@/types";
 export const register = new SlashCommandBuilder()
